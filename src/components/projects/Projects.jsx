dc8a3c504a567e43f870e4937c5eb23f1eb044b0
vargas <vargasvargasjoa@gmail.com>
2025-05-09 15:45:25 -0300

    adjustment

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,20 +4,20 @@ import "./Projects.css";
 const Projects = () => {
     const projects = [
         {
-            title: "Cardápio Virtual",
-            description: "Um sistema simples de cardápio virtual, usando JavaScript, HTML e CSS.",
+            title: "Virtual Menu",
+            description: "A simple virtual menu system using JavaScript, HTML, and CSS.",
             link: "https://moveio.vercel.app/",
             image: "../../assets/moveio.png"
         },
         {
-            title: "Buscador de CEP",
-            description: "Buscador de CEP, usando React.",
+            title: "ZIP Code Finder",
+            description: "A ZIP code finder built with React.",
             link: "https://search-cep-react-omega.vercel.app/",
             image: "../../assets/buscadorDeCep.png"
         },
         {
-            title: "CRUD com Spring Boot",
-            description: "CRUD básico para conhecimento.",
+            title: "CRUD with Spring Boot",
+            description: "Basic CRUD for learning purposes.",
             link: "https://github.com/vargxrz/Spring-Product/tree/master",
             image: "../../assets/crud.png"
         },
@@ -38,7 +38,7 @@ const Projects = () => {
                         {project.image && <img src={project.image} alt={project.title} />}
                         <h4>{project.title}</h4>
                         <p>{project.description}</p>
-                        <div className="project-link">Ver mais</div>
+                        <div className="project-link">Learn more</div>
                     </a>
                 ))}
             </div>
